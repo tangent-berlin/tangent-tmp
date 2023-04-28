e2530346a5a70ecdbf0d1a746420f60060285242
Alex Steinke <recrash@gmail.com>
2023-04-28 23:09:32 +0200

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,17 +44,17 @@ export const metadata: Metadata = {
 	icons: {
 		icon: [
 			{
-				src: "/favicon.ico",
+				url: "/favicon.ico",
 				sizes: "48x48",
 				type: "image/x-icon"
 			},
 			{
-				src: "/favicon-32x32.png",
+				url: "/favicon-32x32.png",
 				sizes: "32x32",
 				type: 'image/png'
 			},
 			{
-				src: "/favicon-16x16.png",
+				url: "/favicon-16x16.png",
 				sizes: "16x16",
 				type: 'image/png'
 			},
